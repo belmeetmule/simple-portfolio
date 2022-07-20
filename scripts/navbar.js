460e487a7a356409e9e62fd67f128be6197f26ea
mule <belmeetmule@gmail.com>
2022-07-21 01:02:33 +0300

    toggle header position style to add blured background

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,16 +2,19 @@ const menu = document.querySelector(".menu")
 const menuItems = document.querySelectorAll(".menuItem")
 const closeIcon = document.querySelector(".closeIcon")
 const menuIcon = document.querySelector(".menuIcon");
+const navHeader = document.querySelector("header")
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
+    navHeader.style.position="fixed"
   } else {
     menu.classList.add("showMenu");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
+    navHeader.style.position="relative"
   }
 }
   closeIcon.addEventListener("click",toggleMenu);
