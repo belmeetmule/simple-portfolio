2312644e10f60d2e5cf0cb796470979e9a9559c4
mule <belmeetmule@gmail.com>
2022-07-24 00:05:18 +0300

    fix linter errors

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -3,11 +3,11 @@ const email = document.getElementById('email');
 const error = document.getElementById('error');
 
 form.addEventListener('submit', (event) => {
-    const messageWhileError = [];
-    if (email.value.match(/[A-Z]/)) {
-        messageWhileError.push('Email should be written in lowercase letters.');
-        error.innerHTML = messageWhileError.join(', ');
-        event.preventDefault();
-        event.stopPropagation();
-    }
+  const messageWhileError = [];
+  if (email.value.match(/[A-Z]/)) {
+    messageWhileError.push('Email should be written in lowercase letters.');
+    error.innerHTML = messageWhileError.join(', ');
+    event.preventDefault();
+    event.stopPropagation();
+  }
 });
