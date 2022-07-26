cdbae672861d1f8297fc41a2ff229bdb85023328
mule <belmeetmule@gmail.com>
2022-07-27 00:02:01 +0300

    fit linter errors

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,23 +1,22 @@
-const menu = document.querySelector('.menu')
-const closeIcon = document.querySelector('.closeIcon')
+const menu = document.querySelector('.menu');
+const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
-const navHeader = document.querySelector('header')
+const navHeader = document.querySelector('header');
 
 function toggleMenu() {
   if (menu.classList.contains('showMenu')) {
     menu.classList.remove('showMenu');
     closeIcon.style.display = 'none';
     menuIcon.style.display = 'block';
-    navHeader.style.position= 'fixed'
+    navHeader.style.position = 'fixed';
   } else {
     menu.classList.add('showMenu');
     closeIcon.style.display = 'block';
     menuIcon.style.display = 'none';
-    navHeader.style.position= 'relative'
+    navHeader.style.position = 'relative';
   }
 }
-  closeIcon.addEventListener('click',toggleMenu);
-  menuIcon.addEventListener('click',toggleMenu);
+closeIcon.addEventListener('click', toggleMenu);
+menuIcon.addEventListener('click', toggleMenu);
 
-  document.querySelectorAll('.menuItem').forEach(i => i.addEventListener('click', toggleMenu)
-  );
+document.querySelectorAll('.menuItem').forEach((i) => i.addEventListener('click', toggleMenu));
